d20c3eb40ee978ed42926c6473ae1c0491903fe2
Shiv Raj Badu <shivrajbadu@gmail.com>
2018-04-06 15:20:38 +0545

    working on unicorn file

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,20 +1,19 @@
-set patth to application
-app_dir = File.expand_path("../..", __FILE__)
+# set path to the application
+app_dir git File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
 working_directory app_dir
 
-
 # Set unicorn options
-# worker_processes 2
-# preload_app true
-# timeout 30
-#
-# # Set up socket location
-# listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
-#
-# # Logging
-# stderr_path "#{shared_dir}/log/unicorn.stderr.log"
-# stdout_path "#{shared_dir}/log/unicorn.stdout.log"
-#
-# # Set master PID location
-# pid "#{shared_dir}/pids/unicorn.pid"
+worker_processes 2
+preload_app true
+timeout 30
+
+# Path for the Unicorn socket
+listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+
+# Set path for logging
+stderr_path "#{shared_dir}/log/unicorn.stderr.log"
+stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+
+# Set proccess id path
+pid "#{shared_dir}/pids/unicorn.pid"
